0edb36c5222eddf227533507a4461b52d67bac33
sphillips <phillips.sb@gmail.com>
2014-10-20 17:58:46 +0200

    added in tappery-site files

---
+++ app/assets/javascripts/navicon.js
@@ -0,0 +1,10 @@
+   $("#nav").addClass("js").before('<div id="menu">&#9776;</div>');
+    $("#menu").click(function(){
+        $("#nav").slideToggle();
+    });
+    $(window).resize(function(){
+        if(window.innerWidth > 768) {
+            $("#nav").removeAttr("style");
+        }
+    });
+  
