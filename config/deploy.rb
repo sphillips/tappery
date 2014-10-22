04a2aaa1f61009482c36dea158974ce1ea18b171
sphillips <phillips.sb@gmail.com>
2014-10-22 13:10:10 +0200

    changed database yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@
 lock '3.2.1'
 
 set :application, 'tappery'
-set :repo_url, 'git@github.com:sphillips/tappery.git'
+set :repo_url, 'https://github.com/sphillips/tappery.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/tappery'
-set :user, 'sean'
+set :user, 'git'
 
 # Default value for :scm is :git
 set :scm, :git
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{git@104.131.200.202}
+role :web, %w{git@104.131.200.202}
+role :db,  %w{git@104.131.200.202}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '104.131.200.202', user: 'sean', roles: %w{web app}
+server '104.131.200.202', user: 'git', roles: %w{web app}
 
 
 # Custom SSH Options
